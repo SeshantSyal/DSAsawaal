d7a06f1eaa4920861f384f03500f3efe6dcd94e0
Seshant Syal <seshantsyal@gmail.com>
2026-09-07 05:31:13 +0530

    Time: 1235 ms (5.00%) | Memory: 55.3 MB (78.15%) - LeetSync

--- 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.cpp
+++ 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.cpp
@@ -11,7 +11,7 @@ public:
         {
             return 0;
         }
-        map <int,int> mp;
+        unordered_map<int,int> mp;
         int l=0;
         int r = 0;
         int n = nums.size();
